06b58aa58c8d4dcce4c3b99e8ac8a2c26f0bcc6b
Cortalo <Cortalo@outlook.com>
2025-11-09 20:36:15 +0100

    starter files, and styled component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <H1>The Wild Oasis</H1>
+      <Button>Check in</Button>
+    </div>
+  );
 }
 
 export default App;
