2927f3eb1957e5a8120a960e0157281199bd05c6
Cortalo <Cortalo@outlook.com>
2025-11-10 21:12:56 +0100

    styled component props and the css function

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
+import Heading from "./ui/Heading";
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: var(--color-brand-600);
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
 `;
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <div>
-        <H1>The Wild Oasis</H1>
-      </div>
+      <StyledApp>
+        <Heading as="h1">The</Heading>
+        <Heading as="h2">The</Heading>
+        <Heading as="h3">The</Heading>
+      </StyledApp>
     </>
   );
 }
